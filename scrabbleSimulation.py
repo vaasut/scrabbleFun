094be35db142b8d9dc89bd0085c578c52176fafc
vaasut <vaasut1@gmail.com>
2024-01-19 01:25:13 -0500

    Add program to simulate draw chance
    Simulate chance of drawing one of a set of words from a given bag of scrabble tiles

---
+++ scrabbleSimulation.py
@@ -0,0 +1,65 @@
+import random, copy
+
+def createScrabbleBag():
+    ones = "KJXQZ"
+    twos = "?BCMPFHVWY"
+    threes= "G"
+    fours = "LSUD"
+    sixes = "NRT"
+    eights = "O"
+    nines = "AI"
+    twelves = "E"
+    scrabbleBag = ones + twos*2 + threes*3 + fours*4 + sixes*6 + eights*8 + nines*9 + twelves*12
+    scrabbleBag = list(scrabbleBag)
+    scrabbleBag.sort()
+    return scrabbleBag
+
+def removeTiles(tiles,scrabbleBag):
+    #will error if tiles are not in bag.
+    for letter in tiles:
+        scrabbleBag.remove(letter)
+    return scrabbleBag
+
+def drawNTiles(n,scrabbleBag):
+    #randomly draw n tiles from bag. does not change bag composition.
+    random.shuffle(scrabbleBag)
+    draw = scrabbleBag[0:n]
+    return draw
+
+def wordInRack(word,rack):
+    #check if word in rack. blanks are ? symbol.
+    rackCopy = copy.deepcopy(rack) #don't modify the rack
+    missing = 0
+    for letter in word:
+        if letter in rackCopy:
+            rackCopy.remove(letter)
+        else:
+            missing += 1
+    for i in range(2):
+        if "?" in rackCopy:
+            rackCopy.remove("?")
+            missing -= 1
+    if missing <= 0:
+        return True
+    return False
+        
+
+def simulateDraw(successDraws,scrabbleBag,iterations):
+    """simulates iterations nnumber of draws from scrabble bag
+    if any of the success draws are drawn, it counts as success"""
+    successCount = 0.0
+    for i in range(iterations):
+        success = False
+        rack = drawNTiles(7,scrabbleBag)
+        for word in successDraws:
+            if wordInRack(word,rack): 
+                success = True # don't double count if two of the successes are drawn together
+        if success:
+            successCount += 1
+    return successCount / iterations
+
+scrabbleBag = createScrabbleBag()
+scrabbleBag = removeTiles("TERWATT",scrabbleBag)
+print(simulateDraw(["GIGA", "NANO", "TERA", "MEGA", "KILO"],scrabbleBag,1000000))
+
+
